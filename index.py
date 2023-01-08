76826a3c81d9ab6e9f3336308c0a77cf0d8cfa64
Ben Smerd <bsmerd@adam.com.au>
2023-01-08 14:00:45 +1030

    install yfinance pip and create user choice for stock input

--- index.py
+++ index.py
@@ -3,3 +3,11 @@
 # show stock prices in a graph
 
 # use this library for the graphs - https://plotly.com/python/financial-charts/
+
+import yfinance as yf
+
+ticker_chosen = input("Type in the ticker for the graph of stock price you would like to view, eg (MSFT= Microsoft, AAPL= Apple, TSLA= Tesla)")
+
+data = yf.Ticker(f"{ticker_chosen}")
+
+print(data.info)
