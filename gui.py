e0b29a75fa021359f8a88b218be84ecf86576cc1
Ben Smerd <bsmerd@adam.com.au>
2023-01-28 18:59:02 +1030

    added in PySimpleGUI and created the layout of the gui

---
+++ gui.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as sg
+
+title_info = sg.Text("Welcome to StockGraph! Input details below and then a graph will be shown for the stock you are tracking.")
+
+#  ? Get the users input for ticker
+ticker = sg.Text("Ticker: ")
+ticker_input = sg.Input()
+
+# ? Get the users input for start and end dates in format
+start_date = sg.Text("Start date (YYYY-MM-DD): ")
+start_date_input = sg.Input()
+end_date = sg.Text("End date (YYYY-MM-DD): ")
+end_date_input = sg.Input()
+
+# ? Button to link up to a function that gets todays date for the input
+end_date_today = sg.Button("Today's Date")
+
+# ? Search button will connect to the function other file and call the API
+submit_button = sg.Button("Search")
+
+window = sg.Window('StockGraph', layout=[
+    [title_info],
+    [ticker, ticker_input],
+    [start_date, start_date_input],
+    [end_date, end_date_input, end_date_today],
+    [submit_button]
+])
+
+event, values = window.read()
+
+print(f"Ticker: {values[0]} Start-Date: {values[1]} End-Date: {values[2]}")
+
+window.close()
