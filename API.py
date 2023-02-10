842246aa86dfd8a910e62a8f9976ff68b5b019f2
Ben Smerd <bsmerd@adam.com.au>
2023-02-10 12:04:50 +1030

    added in date formatting for the y axis on the matplotlib graphs with the imported API dates

--- API.py
+++ API.py
@@ -1,20 +1,11 @@
-
-# ?  get stock prices from--- https://pypi.org/project/yfinance/
-
-# ? user types in a UI what ticker they want and then it shows up a basic graph of the infor for it and then just some basic information underneath it
-#? marketcap, events, link to website, name, ticker, price, percent prices maybe
-
 #?  use this library for the graphs - https://plotly.com/python/financial-charts/
 # https://matplotlib.org/stable/gallery/showcase/stock_prices.html
 
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
-
-# ticker_chosen = input("Type in the ticker for the graph of stock price you would like to view, eg (MSFT= Microsoft, AAPL= Apple, TSLA= Tesla)")
-# data = yf.Ticker(f"{ticker_chosen}")
-
-#!!!! TEMPORARY TESLA SO DONT HAV ETO INPUT EVERYTIME
+import matplotlib.dates as mdates
+import datetime as dt
 
 def call_api(ticker, start_date, end_date, timeframe):
     df = yf.download(ticker, start_date, end_date, interval=timeframe)
@@ -28,10 +19,12 @@ def call_api(ticker, start_date, end_date, timeframe):
         dates.append(date)
         prices.append(row[1])
 
-    # print(dates)
+    formatted_dates = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in dates]
+
+    print(formatted_dates)
     # print(prices)
 
-    graph = create_graph(dates, prices, ticker, start_date, end_date, timeframe)
+    graph = create_graph(formatted_dates, prices, ticker, start_date, end_date, timeframe)
 
 
 
@@ -39,18 +32,22 @@ def create_graph(dates, prices, ticker, start_date, end_date, timeframe):
     # # ? this is the numbers plotting, first array is the X axis (date) second array is the Y axis (price)-- need to get an array of the dates for first, and Highs for second array
     # # * can change style--- have a marker (marker='o', markersize=12)
 
-    date_axis = dates[::100]
-    # print(date_axis)
-
-    plt.plot(dates, prices, color='orange')
-    plt.xticks(range(min(dates), max(dates)+100, 1.0))
-
+    # ? Makes the X axis more readable
     min_price_axis = min(prices) / 4
     max_price_axis = max(prices) + (max(prices) / 8)
 
+    # ? Formats the dates
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+
+    # ? Dynamically renders the Y axis dates according to data input
+    # TODO need to make this dynamic to the timeframe selected by user-- 
+    plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+
+    plt.plot(dates, prices, color='orange')
+    plt.gcf().autofmt_xdate()
 
     # # ? axis numbers, first two are X (date), last 2 are Y (price)
-    # plt.ylim([min_price_axis, max_price_axis])
+    plt.ylim([min_price_axis, max_price_axis])
     # plt.xlim([start_date, end_date])
 
     plt.xlabel(timeframe)
